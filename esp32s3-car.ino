ad5c3dd29d21ccc2eec1929deae1b1f54f432a74
Swarnendu Kundu <swarnendukundu141d@gmail.com>
2026-04-23 12:25:38 +0530

    Reverse motor direction for turning left and right

--- esp32s3-car.ino
+++ esp32s3-car.ino
@@ -85,14 +85,14 @@ void moveBackward() {
 }
 
 void turnLeft() {
-  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
   analogWrite(ENA, currentSpeed); analogWrite(ENB, currentSpeed);
 }
 
 void turnRight() {
-  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
+  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
   analogWrite(ENA, currentSpeed); analogWrite(ENB, currentSpeed);
 }
 
